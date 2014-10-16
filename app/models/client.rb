9bf1a399bf049ffb64c6e9ecc7f51d702ce9c026
Prelang Builder <builder@prelang.com>
2014-10-16 18:50:50 +0000

    Defined validation for attribute 'work_phone' on model 'Client'

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,5 @@ class Client < ActiveRecord::Base
   has_many :visits
   validates_formatting_of :zip_code, using: :us_zip
   validates_formatting_of :cell_phone, using: :us_phone
+  validates_formatting_of :work_phone, using: :us_phone
 end
